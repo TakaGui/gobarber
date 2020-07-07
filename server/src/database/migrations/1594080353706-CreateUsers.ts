6cabed09853beb1c7aa69fd76eac49a50660e71b
TakaGui <gui.yukio175@gmail.com>
2020-07-07 01:22:08 -0300

    fix: alter column type id-varcharToUuid

--- server/src/database/migrations/1594080353706-CreateUsers.ts
+++ server/src/database/migrations/1594080353706-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1594080353706 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
