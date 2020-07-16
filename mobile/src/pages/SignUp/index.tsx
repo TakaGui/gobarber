9510a640ddb71998cf3bd9284cf073c499be631f
TakaGui <gui.yukio175@gmail.com>
2020-07-16 16:40:12 -0300

    add: signup with api

--- mobile/src/pages/SignUp/index.tsx
+++ mobile/src/pages/SignUp/index.tsx
@@ -15,6 +15,8 @@ import * as Yup from 'yup';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/mobile';
 
+import api from '../../services/api';
+
 import getValidationErrors from '../../utils/getValidationErrors';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -52,7 +54,14 @@ const SignUp: React.FC = () => {
         abortEarly: false,
       });
 
-      // await api.post('/users', data);
+      await api.post('/users', data);
+
+      navigation.goBack();
+
+      Alert.alert(
+        'Cadastro realizado com sucesso!',
+        'Você  já pode fazer login na aplicação.',
+      );
     } catch (err) {
       if (err instanceof Yup.ValidationError) {
         const errors = getValidationErrors(err);
