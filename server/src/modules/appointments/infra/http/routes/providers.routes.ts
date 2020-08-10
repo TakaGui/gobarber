da1a9cfdee61eec7e351f174a59119c5745322c1
TakaGui <gui.yukio175@gmail.com>
2020-08-10 16:00:35 -0300

    add: route list provider month & day availability

--- server/src/modules/appointments/infra/http/routes/providers.routes.ts
+++ server/src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -2,12 +2,27 @@ import { Router } from 'express';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import ProvidersController from '../controllers/ProvidersController';
+import ProviderMonthAvailabilitiesController from '../controllers/ProviderMonthAvailabilitiesController';
+import ProviderDayAvailabilitiesController from '../controllers/ProviderDayAvailabilitiesController';
 
 const providersRouter = Router();
+
 const providersController = new ProvidersController();
+const providerMonthAvailabilitiesController = new ProviderMonthAvailabilitiesController();
+const providerDayAvailabilitiesController = new ProviderDayAvailabilitiesController();
 
 providersRouter.use(ensureAuthenticated);
 
 providersRouter.get('/', providersController.index);
+
+providersRouter.get(
+  '/:provider_id/month-availability',
+  providerMonthAvailabilitiesController.index,
+);
+
+providersRouter.get(
+  '/:provider_id/day-availability',
+  providerDayAvailabilitiesController.index,
+);
 
 export default providersRouter;
