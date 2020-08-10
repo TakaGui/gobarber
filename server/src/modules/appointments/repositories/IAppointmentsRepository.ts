56f2e40fe8d3220588e1adc229c06261ab3b450b
TakaGui <gui.yukio175@gmail.com>
2020-08-10 11:19:40 -0300

    add: data find all provider in day

--- server/src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ server/src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,11 +1,15 @@
 import Appointment from '../infra/typeorm/entities/Appointment';
 import ICreateAppointmentDTO from '../dtos/ICreateAppointmentDTO';
 import IFindAllInMonthFromProviderDTO from '../dtos/IFindAllInMonthFromProviderDTO';
+import IFindAllInDayProviderDTO from '../dtos/IFindAllInDayFromProviderDTO';
 
 export default interface IAppointmentsRepository {
   create(data: ICreateAppointmentDTO): Promise<Appointment>;
   findByDate(date: Date): Promise<Appointment | undefined>;
   findAllInMonthFromProvider(
     data: IFindAllInMonthFromProviderDTO,
+  ): Promise<Appointment[]>;
+  findAllInDayFromProvider(
+    data: IFindAllInDayProviderDTO,
   ): Promise<Appointment[]>;
 }
