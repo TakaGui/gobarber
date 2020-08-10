50653d05695104d9730fbace0f108c3fa044ca7f
TakaGui <gui.yukio175@gmail.com>
2020-08-10 13:41:43 -0300

    add: test create an appointment on a past date

--- server/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ server/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -15,8 +15,15 @@ describe('CreateAppointment', () => {
   });
 
   it('should be able to create a new appointment', async () => {
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 4, 10, 12).getTime();
+    });
+
+    // appointmentDate: 2020/05/10 13:00:00
+    const appointmentDate = new Date(2020, 4, 10, 13);
+
     const appointment = await createAppointment.execute({
-      date: new Date(),
+      date: appointmentDate,
       user_id: '132123',
       provider_id: '123321',
     });
@@ -26,8 +33,12 @@ describe('CreateAppointment', () => {
   });
 
   it('should not be able to create two appointments on the same time', async () => {
-    // appointmentDate: 2020/05/10 11:00:00
-    const appointmentDate = new Date(2020, 4, 10, 11);
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 4, 10, 12).getTime();
+    });
+
+    // appointmentDate: 2020/05/10 13:00:00
+    const appointmentDate = new Date(2020, 4, 10, 12);
 
     await createAppointment.execute({
       date: appointmentDate,
@@ -38,6 +49,20 @@ describe('CreateAppointment', () => {
     await expect(
       createAppointment.execute({
         date: appointmentDate,
+        user_id: '132123',
+        provider_id: '123321',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to create an appointment on a past date', async () => {
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 4, 10, 12).getTime();
+    });
+
+    await expect(
+      createAppointment.execute({
+        date: new Date(2020, 4, 10, 11),
         user_id: '132123',
         provider_id: '123321',
       }),
