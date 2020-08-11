332e544f6dae2abcc6840a891a86655051928419
TakaGui <gui.yukio175@gmail.com>
2020-08-11 12:36:30 -0300

    refactor:Provider upload config driver s3 &  disk

--- server/src/shared/container/providers/StorageProvider/index.ts
+++ server/src/shared/container/providers/StorageProvider/index.ts
@@ -1,4 +1,5 @@
 import { container } from 'tsyringe';
+import uploadConfig from '@config/upload';
 
 import IStorageProvider from './models/IStorageProvider';
 
@@ -6,11 +7,11 @@ import DiskStorageProvider from './implementations/DiskStorageProvider';
 import S3StorageProvider from './implementations/S3StorageProvider';
 
 const providers = {
-  diskStorage: DiskStorageProvider,
-  s3Storage: S3StorageProvider,
+  disk: DiskStorageProvider,
+  s3: S3StorageProvider,
 };
 
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
-  providers.diskStorage,
+  providers[uploadConfig.driver],
 );
