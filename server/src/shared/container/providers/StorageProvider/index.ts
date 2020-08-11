b7a1bd08f695527d3a79f6fb6fbf667ebeb42a22
TakaGui <gui.yukio175@gmail.com>
2020-08-11 11:19:54 -0300

    add: import s3 storage provider

--- server/src/shared/container/providers/StorageProvider/index.ts
+++ server/src/shared/container/providers/StorageProvider/index.ts
@@ -3,9 +3,11 @@ import { container } from 'tsyringe';
 import IStorageProvider from './models/IStorageProvider';
 
 import DiskStorageProvider from './implementations/DiskStorageProvider';
+import S3StorageProvider from './implementations/S3StorageProvider';
 
 const providers = {
   diskStorage: DiskStorageProvider,
+  s3Storage: S3StorageProvider,
 };
 
 container.registerSingleton<IStorageProvider>(
