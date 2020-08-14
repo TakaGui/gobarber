0e1eec17c57d0fa18fd6fed09afbc815127dc0ef
TakaGui <gui.yukio175@gmail.com>
2020-08-14 18:18:43 -0300

    add: format date

--- mobile/src/pages/AppointmentCreated/index.tsx
+++ mobile/src/pages/AppointmentCreated/index.tsx
@@ -1,7 +1,9 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useMemo } from 'react';
 import Icon from 'react-native-vector-icons/Feather';
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 
 import {
   Container,
@@ -11,8 +13,15 @@ import {
   OkButtonText,
 } from './styles';
 
+interface IRouteParams {
+  date: number;
+}
+
 const AppointmentCreated: React.FC = () => {
   const { reset } = useNavigation();
+  const { params } = useRoute();
+
+  const routeParams = params as IRouteParams;
 
   const handleOkPresses = useCallback(() => {
     reset({
@@ -21,12 +30,21 @@ const AppointmentCreated: React.FC = () => {
     });
   }, [reset]);
 
+  const formattedDate = useMemo(() => {
+    return format(
+      routeParams.date,
+      // Terça, dia 14 de março de 2020 às 12h00
+      "EEEE', dia ' dd ' de ' MMMM ' de ' yyyy ' às ' HH:mm",
+      { locale: ptBR },
+    );
+  }, [routeParams.date]);
+
   return (
     <Container>
       <Icon name="check" size={80} color="#04d361" />
 
       <Title>Agendamento concluído</Title>
-      <Description>Terça, dia 14 de março de 2020 às 12h00</Description>
+      <Description>{formattedDate}</Description>
 
       <OkButton onPress={handleOkPresses}>
         <OkButtonText>Ok</OkButtonText>
