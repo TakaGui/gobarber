3bc91d876ca7fd73c8b56edd1b44efc82999f23a
TakaGui <gui.yukio175@gmail.com>
2020-08-14 18:04:04 -0300

    add: navigate reset press button ok

--- mobile/src/pages/AppointmentCreated/index.tsx
+++ mobile/src/pages/AppointmentCreated/index.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import Icon from 'react-native-vector-icons/Feather';
+
+import { useNavigation } from '@react-navigation/native';
 
 import {
   Container,
@@ -10,6 +12,15 @@ import {
 } from './styles';
 
 const AppointmentCreated: React.FC = () => {
+  const { reset } = useNavigation();
+
+  const handleOkPresses = useCallback(() => {
+    reset({
+      routes: [{ name: 'Dashboard' }],
+      index: 0,
+    });
+  }, [reset]);
+
   return (
     <Container>
       <Icon name="check" size={80} color="#04d361" />
@@ -17,7 +28,7 @@ const AppointmentCreated: React.FC = () => {
       <Title>Agendamento concluído</Title>
       <Description>Terça, dia 14 de março de 2020 às 12h00</Description>
 
-      <OkButton onPress={() => {}}>
+      <OkButton onPress={handleOkPresses}>
         <OkButtonText>Ok</OkButtonText>
       </OkButton>
     </Container>
