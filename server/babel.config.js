064836b72ad61084913f8340cc00c2678d89c764
TakaGui <gui.yukio175@gmail.com>
2020-08-17 15:28:16 -0300

    add: plugins

--- server/babel.config.js
+++ server/babel.config.js
@@ -11,5 +11,8 @@ module.exports = {
         "@shared": "./src/shared",
       }
     }],
+    "babel-plugin-transform-typescript-metadata",
+    ["@babel/plugin-proposal-decorators", { "legacy": true }],
+    ["@babel/plugin-proposal-class-properties", { "loose": true }],
   ],
 }
